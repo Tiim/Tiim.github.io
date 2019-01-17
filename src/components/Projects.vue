519619b8bb042f8ea3a99b26d971ce0b1fcf9d83
Tiim <tim.bachmann96@gmail.com>
2019-01-17 15:24:01 +0100

    Add projects

---
+++ src/components/Projects.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div v-for="row in cols" :key="row.id" class="columns">
+      <div v-for="project in row" :key="project.id" class="column is-one-third">
+        <Project :project="project"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Project from './Project';
+import chunk from '../utils/chunk';
+
+import projectsData from '../data/projects.json';
+
+export default {
+  name: 'Projects',
+  components: {
+    Project,
+  },
+  data() {
+    return {
+      projects: projectsData,
+    };
+  },
+  computed: {
+    cols() {
+      return chunk(this.projects, 3);
+    },
+  },
+};
+</script>
--- src/views/ProjectsPage.vue
+++ src/views/ProjectsPage.vue
@@ -4,11 +4,18 @@
       <h1>Projects</h1>
       <i>Under construction</i>
     </div>
+    <div class="section content">
+      <Projects/>
+    </div>
   </div>
 </template>
 
 <script>
+import Projects from '../components/Projects';
 export default {
   name: 'ProjectsPage',
+  components: {
+    Projects,
+  },
 };
 </script>
