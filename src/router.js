5a2c7ebf932cc423584f1993fbbb42e084c9fa31
Tiim <tim.bachmann96@gmail.com>
2019-01-17 16:01:16 +0100

    Dynamically load all pages

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HomePage from './views/HomePage.vue';
 
 Vue.use(Router);
 
@@ -11,7 +10,8 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: HomePage,
+      component: () =>
+        import(/* webpackChunkName: "about" */ './views/HomePage.vue'),
     },
     {
       path: '/projects',
