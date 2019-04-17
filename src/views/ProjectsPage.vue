292cbf50348a50ac0b55944320a1360fc275decf
Tiim <tim.bachmann96@gmail.com>
2019-04-17 10:37:35 +0200

    Add "demos" section to the projects page

--- src/views/ProjectsPage.vue
+++ src/views/ProjectsPage.vue
@@ -1,24 +1,35 @@
 <template>
   <div>
     <div class="section content">
-      <h1>Projects⚙️</h1>
+      <h1>Projects &amp; Demos⚙️</h1>
       <p>This is a list of all my current or past projects. Go check them out!</p>
-      <p>
-        <i>This page is not complete yet.</i>
-      </p>
     </div>
     <div class="section content">
-      <Projects/>
+      <h2>Projects</h2>
+      <Projects :data="projectsData"/>
+    </div>
+    <div class="section content">
+      <h2>Demos</h2>
+      <Projects :data="demosData"/>
     </div>
   </div>
 </template>
 
 <script>
+import projectsData from '../data/projects';
+import demosData from '../data/demos';
+
 import Projects from '../components/Projects';
 export default {
   name: 'ProjectsPage',
   components: {
     Projects,
+  },
+  data() {
+    return {
+      projectsData,
+      demosData,
+    };
   },
 };
 </script>
