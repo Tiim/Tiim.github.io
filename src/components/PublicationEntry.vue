ebbece23f7f0a456807859336176f4e83ca7972e
Guy Meyer <guymeyer1995@gmail.com>
2020-07-25 13:01:08 -0400

    add mobile support

--- src/components/PublicationEntry.vue
+++ src/components/PublicationEntry.vue
@@ -1,17 +1,17 @@
 <template>
 <div>
 
-  <article class="card">
+  <article class="card large-screen">
     <div class="card-content has-text-left">
       <table style="width:100%">
-    <tr>
-      <th>{{ publication.date }}</th>
-      <th>{{ publication.name }}</th>
-      <th>
-        <a :href="publication.source" target="_blank">Read ...</a>
-      </th>
-    </tr>
-  </table>
+        <tr>
+          <th>{{ publication.date }}</th>
+          <th>{{ publication.name }}</th>
+          <th>
+            <a :href="publication.source" target="_blank">Read ...</a>
+          </th>
+        </tr>
+      </table>
       <div class="tags">
         <span v-for="tag in publication.tags" :key="tag" class="tag">
           {{ tag }}
@@ -19,6 +19,36 @@
       </div>
     </div>
   </article>
+
+  <article class="card small-screen">
+    <div class="card-content has-text-left">
+      <table style="width:100%">
+        <tr>
+          <!-- <th>{{ publication.date }}</th> -->
+          <th>{{ publication.name }}</th>
+          <!-- <th>
+            <a :href="publication.source" target="_blank">Read ...</a>
+          </th> -->
+        </tr>
+      </table>
+      <div class="tags">
+        <span v-for="tag in publication.tags" :key="tag" class="tag">
+          {{ tag }}
+        </span>
+      </div>
+
+    <div class="card-footer">
+      <a
+        :href="publication.source"
+        class="card-footer-item"
+        target="_blank"
+      >
+        More ...
+      </a>
+    </div>
+    </div>
+  </article>
+
 </div>
 </template>
 
@@ -30,3 +60,41 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+
+
+@media screen and (min-width: 1000px) {
+  .large-screen {
+    visibility: visible;
+    /* display: flex; */
+  }
+
+  .small-screen {
+    visibility: hidden;
+    display: none;
+    clear: both;
+    float: left;
+  }
+}
+
+@media screen and (max-width: 1000px) {
+  .large-screen {
+    visibility: hidden;
+    display: none;
+    clear: both;
+    float: left;
+  }
+
+  .small-screen {
+    visibility: visible;
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  .project-item {
+    margin-bottom: 10%;
+  }
+}
+
+</style>
