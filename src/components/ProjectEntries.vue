af567322b81a1c38440cd71f2f1ec11d8d78ad43
Guy Meyer <guymeyer1995@gmail.com>
2020-07-25 12:51:52 -0400

    add mobile support

--- src/components/ProjectEntries.vue
+++ src/components/ProjectEntries.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
-    <div class="row" :key="i" v-for="i in rowCount">
+    <div class="row large-screen" :key="i" v-for="i in rowCount">
       <span
         v-for="project in itemCountInRow(i)"
         :key="project.id"
         class="column is-one-third is-multiline"
         style="margin: 0;"
+      >
+        <Project :project="project.node" />
+      </span>
+    </div>
+
+    <div class="column small-screen">
+      <span
+        v-for="project in this.data"
+        :key="project.id"
+        class="project-item is-one-third is-multiline"
       >
         <Project :project="project.node" />
       </span>
@@ -43,8 +53,38 @@ export default {
 
 <style scoped>
 
-.row {
-  display: flex;
+
+@media screen and (min-width: 600px) {
+  .large-screen {
+    visibility: visible;
+    display: flex;
+  }
+
+  .small-screen {
+    visibility: hidden;
+    display: none;
+    clear: both;
+    float: left;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .large-screen {
+    visibility: hidden;
+    display: none;
+    clear: both;
+    float: left;
+  }
+
+  .small-screen {
+    visibility: visible;
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  .project-item {
+    margin-bottom: 10%;
+  }
 }
 
 </style>
