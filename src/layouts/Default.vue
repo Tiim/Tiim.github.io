e9f89e6587a13c090f32bfd1bab6e6a18b54089e
Guy Meyer <guymeyer1995@gmail.com>
2020-07-25 11:35:27 -0400

    remove footer

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -4,15 +4,11 @@
     <div class="section">
       <slot />
     </div>
-    <div class="section has-text-centered">
-      <Footer />
-    </div>
   </div>
 </template>
 
 <script>
 import Header from "../components/Header";
-import Footer from "../components/Footer";
 export default {
   name: "App",
   components: {
