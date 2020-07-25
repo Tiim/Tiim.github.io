526f0ea84e909a7d35de2ea6372a254490eaca18
Guy Meyer <guymeyer1995@gmail.com>
2020-07-25 11:24:09 -0400

    add publication pages

---
+++ src/components/PublicationEntry.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+
+  <article class="card">
+    <div class="card-content has-text-left">
+      <table style="width:100%">
+    <tr>
+      <th>{{ publication.date }}</th>
+      <th>{{ publication.name }}</th>
+      <th>
+        <a :href="publication.source" target="_blank">Read ...</a>
+      </th>
+    </tr>
+  </table>
+      <div class="tags">
+        <span v-for="tag in publication.tags" :key="tag" class="tag">
+          {{ tag }}
+        </span>
+      </div>
+    </div>
+  </article>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Publications",
+  props: {
+    publication: Object
+  }
+};
+</script>
---
+++ src/pages/Publications.vue
@@ -0,0 +1,45 @@
+<template>
+  <Layout>
+    <div class="container has-text-centered">
+      <div class="section content">
+        <h1>Thesis 📖</h1>
+      </div>
+      <div class="section content">
+        <div
+          v-for="publication in $page.publications.edges"
+          :key="publication.node.id"
+          class="column"
+        >
+          <PublicationEntry :publication="publication.node" />
+        </div>
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<script>
+import PublicationEntry from "~/components/PublicationEntry";
+export default {
+  name: "PublicationsPage",
+  components: { PublicationEntry },
+  metaInfo: {
+    title: "Publication"
+  }
+};
+</script>
+<page-query>
+  query {
+    publications: allPublication(sortBy: "date", order: ASC) {
+      edges {
+        node {
+          id
+          name
+          source
+          date
+          text
+          tags
+        }
+      }
+    }
+  }
+</page-query>
