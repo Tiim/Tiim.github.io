a7e6783265d886e9202ebc855cf9c24228e12aa1
Tiim <tim.bachmann96@gmail.com>
2022-04-27 11:13:03 +0200

    Switch rss library

--- src/routes/blog/rss.xml.js
+++ src/routes/blog/rss.xml.js
@@ -1,6 +1,7 @@
 import fs from "fs-extra";
 import { process } from "$lib/markdown.js";
-import rss from "rss";
+import { Feed } from "feed";
+import remoteImages from "$content/remote-images.json";
 
 export async function get() {
   const dir = await fs.readdir(`content/blog`);
@@ -16,40 +17,49 @@ export async function get() {
   ).filter((p) => p);
   posts.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
 
-  var feed = new rss({
+  var feed = new Feed({
     title: "Tim Bachmann's Blog",
     description:
       "Blog about web development, programming, and anything that might interest me.",
-    feed_url: "https://tiim.ch/blog/rss.xml",
-    site_url: "https://tiim.ch/",
-    image_url: "https://github.com/Tiim.png",
-    managingEditor: "Tim Bachmann",
-    webMaster: "Tim Bachmann",
-    copyright: "Tim Bachmann",
+    id: "https://tiim.ch/",
+    link: "https://tiim.ch/",
     language: "en",
-    pubDate: new Date(),
-    ttl: 60 * 24,
+    image: remoteImages.profilePicture.link,
+    favicon: "https://tiim.ch/favicon.ico",
+    copyright: "Tim Bachmann",
+    updated: new Date(),
+    feedLinks: {
+      atom: "https://tiim.ch/blog/rss.xml",
+      //json: "https://tiim.ch/blog/rss.json",
+    },
+    author: {
+      name: "Tim Bachmann",
+      email: "hey@tiim.ch",
+      link: "https://tiim.ch/",
+    }
   });
 
   posts.forEach((post) => {
-    feed.item({
+    feed.addItem({
       title: post.title,
+      id: `https://tiim.ch/${post.slug}`,
+      link: `https://tiim.ch/${post.slug}`,
       description: post.description,
-      url: `https://tiim.ch/${post.slug}`,
-      categories: post.tags,
-      author: "Tim Bachmann",
-      date: post.date,
-      custom_elements: [
+      content: post.html,
+      category: post.tags.map((t) => ({ name: t, term: t })),
+      author: [
         {
-          "content:encoded": {
-            _cdata: post.html,
-          },
+          name: "Tim Bachmann",
+          email: "hey@tiim.ch",
+          link: "https://tiim.ch/",
         },
       ],
+      date: new Date(post.date),
+      image: `https://tiim.ch${post.cover_image}`,
     });
   });
 
   return {
-    body: feed.xml({ indent: true }),
+    body: feed.rss2(),
   };
 }
